815a686d6b85ded1104ea7466d89811119c361dc
JUYEONGWOO <yeongdue@gmail.com>
2020-11-04 14:57:02 +0900

    oracle 수정1

--- scott1104.sql
+++ scott1104.sql
@@ -1,1 +1,11 @@
+-- scott 계정이 가지고 잇는 모든 테이블 보기
 select * from tab;
+-- emp 테이블의 모든 내용 조회
+SELECT * FROM EMP;
+-- emp 테이블의 특정 필드만 조회
+SELECT ENAME,job,sal from emp;
+-- 중복 레코드 제거한 후 조회
+select distinct deptno from emp;
+
+-- emp 테이블의 구조
+--desc emp;
---
+++ system1104.sql
@@ -0,0 +1,7 @@
+-- scott 계정 암호 지정한 후 계정 해제하기
+
+alter user scott identified by tiger account unlock;
+
+-- hr 계정 암호 지정한 후 계정 해제하기
+
+alter user hr identified by 12345 account unlock;
